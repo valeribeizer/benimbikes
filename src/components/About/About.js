874d8c36c50d5ce8e0fe3fd09a5f6668862d1b3c
Valeryia Beizer <strelcova.lera94@gmail.com>
2024-01-18 09:51:48 +0100

    change About section text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,11 +18,22 @@ const About = () => {
         <h2>About us</h2>
         <div className="aboutInfoContainer">
           <p>
-            We are passionate about making a difference in the world, and we
-            donate a portion of our profits to organizations that promote peace,
-            love, and environmental conservation. By choosing Benimbikes, you
-            are not only getting a great bike but also contributing to a better
-            world.
+            Benimbikes is a small family business located in Stockholm,
+            Hägersten. We are passionate about bikes and dedicated to providing
+            high-quality products and services to our customers.
+            <br />
+            <br />
+            As a small family business, we take pride in our personalized
+            approach and attention to detail. We believe in building strong
+            relationships with our customers and providing them with the best
+            possible experience. When you choose Benimbikes, you can expect
+            friendly and knowledgeable service from our team.
+            <br />
+            <br /> We are passionate
+            about making a difference in the world, and we donate a portion of
+            our profits to organizations that promote peace, love, and
+            environmental conservation. By choosing Benimbikes, you are not only
+            getting a great bike but also contributing to a better world.
             <br />
             <br /> Join us in spreading peace, love, and bikes. Together, we can
             make a positive impact and create a more harmonious world.
